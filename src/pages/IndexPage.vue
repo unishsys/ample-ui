37925bfc2d18e6dd3591bb76cf0c8cf21f173a2a
by-sabbir <me@sabbir.dev>
2025-01-11 00:28:30 +0600

    feat: updated node 22

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -8,20 +8,31 @@
       </q-card-section>
 
       <q-card-section class="chat-section">
-
-        <div v-for="(message, index) in messages" :key="index"
-          :class="{ 'user-message': message.isUser, 'bot-message': !message.isUser }">
+        <div
+          v-for="(message, index) in messages"
+          :key="index"
+          :class="{
+            'user-message': message.isUser,
+            'bot-message': !message.isUser,
+          }"
+        >
           <p v-if="message.isUser">{{ message.text }}</p>
           <p v-else v-html="message.text"></p>
-
         </div>
-        <div v-if="streamText != '__Sorry Something is wrong__'" class="bot-message">
+        <div
+          v-if="streamText != '__Sorry Something is wrong__'"
+          class="bot-message"
+        >
           <p v-html="streamText"></p>
         </div>
       </q-card-section>
 
       <q-card-section class="input-section">
-        <q-input v-model="userInput" placeholder="Type your message..." @keyup.enter="sendMessage"></q-input>
+        <q-input
+          v-model="userInput"
+          placeholder="Type your message..."
+          @keyup.enter="sendMessage"
+        ></q-input>
         <q-btn label="Send" @click="sendMessage"></q-btn>
       </q-card-section>
     </q-card>
@@ -29,16 +40,14 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { Notify } from 'quasar'
-import { marked } from 'marked';
-import hljs from 'highlight.js';
+import { ref } from "vue";
+import { Notify } from "quasar";
+import { marked } from "marked";
+import hljs from "highlight.js";
 
-
-const streamText = ref('__Sorry Something is wrong__')
-const userInput = ref('')
-const messages = ref([])
-
+const streamText = ref("__Sorry Something is wrong__");
+const userInput = ref("");
+const messages = ref([]);
 
 marked.setOptions({
   highlight: function (code, lang) {
@@ -48,33 +57,33 @@ marked.setOptions({
     return hljs.highlightAuto(code).value;
   },
   breaks: true,
-  gfm: true
+  gfm: true,
 });
 
 async function sendMessage() {
-  if (userInput.value.trim() !== '') {
-    messages.value.push({ text: userInput.value, isUser: true })
-    let userMessage = userInput.value
-    userInput.value = ''
+  if (userInput.value.trim() !== "") {
+    messages.value.push({ text: userInput.value, isUser: true });
+    let userMessage = userInput.value;
+    userInput.value = "";
 
-    const botMessage = { text: '', isUser: false }
-    messages.value.push(botMessage)
+    const botMessage = { text: "", isUser: false };
+    messages.value.push(botMessage);
 
     try {
-      const url = 'https://chatapi.sabbir.dev/chat/77252db7-0e8e-4f41-a9f3-aa42688a6481';
+      const url = "/chat/54cac828-a410-4662-a615-c1ff16e395c4";
       const options = {
-        method: 'POST',
-        headers: { 'content-type': 'application/json' },
+        method: "POST",
+        headers: { "content-type": "application/json" },
         body: JSON.stringify({
-          "prompt": userMessage
-        })
+          prompt: userMessage,
+        }),
       };
 
-      console.log(options)
+      console.log(options);
       const response = await fetch(url, options);
 
       if (!response.body) {
-        throw new Error('ReadableStream not supported by the browser.');
+        throw new Error("ReadableStream not supported by the browser.");
       }
       if (response.status != 200) {
         const data = await response.json();
@@ -82,14 +91,14 @@ async function sendMessage() {
         Notify.create({
           message: data.error,
           color: "red-6",
-          position: "top-right"
-        })
+          position: "top-right",
+        });
       }
       const reader = response.body.getReader();
-      const decoder = new TextDecoder('utf-8');
+      const decoder = new TextDecoder("utf-8");
       let done = false;
 
-      streamText.value = ""
+      streamText.value = "";
       while (!done) {
         const { value, done: readerDone } = await reader.read();
         if (readerDone) break;
@@ -100,37 +109,33 @@ async function sendMessage() {
 
           if (json.response) {
             try {
-
               streamText.value += json.response;
               botMessage.value.text += json.response;
             } catch (error) {
-              console.log(error)
+              console.log(error);
             }
           }
           done = json.done;
           if (done) {
-
           }
         } catch (error) {
-          console.error('Error parsing JSON chunk:', error);
+          console.error("Error parsing JSON chunk:", error);
         }
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   }
-  messages.value.push({ text: marked(streamText.value), isUser: false })
-  console.log(messages)
-  streamText.value = "__Sorry Something is wrong__"
+  messages.value.push({ text: marked(streamText.value), isUser: false });
+  console.log(messages);
+  streamText.value = "__Sorry Something is wrong__";
 
-  messages.value = messages.value.filter(message => message.text !== "");
-
+  messages.value = messages.value.filter((message) => message.text !== "");
 }
-
 </script>
 
 <style scoped>
-@import 'highlight.js/styles/github-dark.css';
+@import "highlight.js/styles/github-dark.css";
 
 .chat-card {
   width: 100%;
